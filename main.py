86900594f59e5bde693a3f94d1c7189c9fa2ebad
arihant1805 <22cs3016@rgipt.ac.in>
2025-05-21 11:50:34 +0530

    Sily chnage

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def chat():
     """
     This function is used to chat with the Vitteey Sahaayak.
     """
-    print("Hello, I am Vitteey Sahaayak, your financial helper\n\nhow can I help you today? (type '000' to quit)i")
+    print("Vitteey Sahaayak: Hello, I am Vitteey Sahaayak, your financial helper\n\nhow can I help you today? (type '000' to quit)")
 
     while True:
         user_input = input("\nYou: ")
         if user_input.lower() == "000":
-            print("Goodbye!")
+            print("Vitteey Sahaayak: Goodbye!")
             break
         else:
             Financial_Agent.print_response(user_input, stream=True)
